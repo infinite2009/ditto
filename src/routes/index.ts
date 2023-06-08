1d82bd4febd37a2b9a22721c26e1b574bccabc48
infinite2009 <455429698@qq.com>
2023-06-09 00:20:05 +0800

    chore: add some routes

---
+++ src/routes/index.ts
@@ -0,0 +1,15 @@
+const routes = [{
+    path: '/project-manage',
+    name: '项目管理'
+}, {
+    path: '/page-edit',
+    name: '页面编辑'
+}, {
+    path: '/page-preview',
+    name: '预览'
+}, {
+    path: '/home',
+    name: '首页'
+}]
+
+export default routes;
