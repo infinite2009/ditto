b6a865d4eac1c69d4a3610bfe642f0b5de044a8c
infinite2009 <455429698@qq.com>
2023-06-21 00:09:09 +0800

    chore: partial commit

--- src/types/handler.schema.ts
+++ src/types/handler.schema.ts
@@ -1,12 +1,15 @@
+import ISchema from "@/types/schema";
+
 /**
  * @file 事件响应函数定义
  * @description 还没有想好怎么用
  */
-export default interface IHandlerSchema {
+export default interface IHandlerSchema extends ISchema {
   name: string;
   desc: string;
-  type: 'arrow' | 'traditional';
-  body: {
+  functionType: 'arrow' | 'traditional';
+  // 函数签名
+  signature: {
     funcName: string;
     params: {
       name: string;
--- src/types/schema.ts
+++ src/types/schema.ts
@@ -2,5 +2,5 @@ export default interface ISchema {
   // 通过 nano id 生成
   id: string;
   // schema 类型：页面、组件、模板
-  type: 'page' | 'component' | 'template',
+  type: 'page' | 'component' | 'template' | 'event' | 'handler' | 'action',
 }
