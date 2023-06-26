332e9a408e9555138bade26faa3129aafc2dad9e
luodongyang <luodongyang@bilibili.com>
2023-06-26 11:19:17 +0800

    chore: add comment

--- src/types/page.schema.ts
+++ src/types/page.schema.ts
@@ -36,5 +36,6 @@ export default interface IPageSchema extends ISchema {
   service: DynamicObject;
   // 动作函数：转移状态、变换和格式化数据（最好放到 BFF 层），调用服务
   actions: DynamicObject<IActionSchema>;
+  // 页面内的事件，页面载入、退出、滚动、周期性事件
   events: DynamicObject<IEventSchema>
 }
