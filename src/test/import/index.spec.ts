69e4d0c3398d3ee142cb537f6bee6e10c9c954f9
luodongyang <luodongyang@bilibili.com>
2023-06-28 09:54:14 +0800

    refactor: renaming

--- src/test/import/index.spec.ts
+++ src/test/import/index.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import TypeScriptCodeGenerator, { IImportOptions } from '@/service/code-generator/react';
+import TypeScriptCodeGenerator, { IImportOptions } from '@/service/code-generator/typescript';
 
 describe('import code generator', () => {
   test('object import', () => {
