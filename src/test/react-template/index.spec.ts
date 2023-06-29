0346f466fa577ee89ff6b105e8d40f0f29900e5f
luodongyang <luodongyang@bilibili.com>
2023-06-29 14:41:26 +0800

    chore: add test case

--- src/test/react-template/index.spec.ts
+++ src/test/react-template/index.spec.ts
@@ -1,7 +1,53 @@
 import { describe, expect } from '@jest/globals';
+import ReactCodeGenerator, { IPropsOptions, ITSXOptions } from '@/service/code-generator/react';
+import IPageSchema from '@/types/page.schema';
+import dsl from '@/mock/tab-case.json';
 
 describe('react-template', () => {
-  test('should return ', ()=> {
-    expect(1).toBe(1);
-  })
+  const react = new ReactCodeGenerator(dsl as unknown as IPageSchema);
+  test('should return props string', () => {
+    const propsOpt: IPropsOptions = {
+      name: 'title',
+      variableName: 'buttonTitle',
+      variableType: 'string',
+      variableValueSource: 'fixed'
+    };
+    expect(react.generatePropStr(propsOpt)).toStrictEqual('title={buttonTitle}');
+  });
+  test('should return template sentences array', () => {
+    const tsx: ITSXOptions = {
+      componentName: 'div',
+      children: [
+        {
+          componentName: 'Button',
+          propsStrArr: ['title={buttonTitle}', 'onClick={handleClicking}']
+        },
+        {
+          componentName: 'Input.Search',
+          propsStrArr: ['value={inputValue}']
+        },
+        {
+          componentName: 'div',
+          children: [
+            {
+              componentName: 'div'
+            },
+            {
+              componentName: 'p'
+            }
+          ]
+        }
+      ]
+    };
+    expect(react.generateTSX(tsx)).toStrictEqual([
+      '<div >',
+      '<Button title={buttonTitle} onClick={handleClicking} />',
+      '<Input.Search value={inputValue} />',
+      '<div >',
+      '<div />',
+      '<p />',
+      '</div>',
+      '</div>'
+    ]);
+  });
 });
