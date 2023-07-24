cf6418d610b3e200cfa50a6074eebd1d5d0f876e
luodongyang <luodongyang@bilibili.com>
2023-07-24 11:14:04 +0800

    chore: add folder

--- src/pages/editor/ page-panel/index.tsx
+++ src/pages/editor/ page-panel/index.tsx
@@ -2,8 +2,14 @@ import { useEffect, useState } from 'react';
 import { Menu, Tree } from 'antd';
 import { FileOutlined, FolderOutlined } from '@ant-design/icons';
 
+interface PageData {
+  key: string;
+  title: string;
+  children?: PageData[];
+}
+
 export default function PagePanel() {
-  const [pages, setPages] = useState<any[]>([]);
+  const [pages, setPages] = useState<PageData[]>([]);
 
   useEffect(() => {
     fetchPages();
@@ -11,27 +17,38 @@ export default function PagePanel() {
 
   function fetchPages() {
     setPages([{
-      key: '123',
-      label: '文件夹1',
-      icon: <FolderOutlined />,
+      key: '1.2.0',
+      title: '1.2.0 迭代',
       children: [{
-        key: '456',
-        label: '页面1',
-        icon: <FileOutlined />
+        key: '1.2.0-1',
+        title: '核心链路监控',
+      }]
+    }, {
+      key: '1.2.1',
+      title: '1.2.1 迭代',
+      children: [{
+        key: '1.2.1-1',
+        title: '核心链路监控改版'
       }]
     }]);
   }
 
+  function handlingSelect() {
+    console.log('page selected');
+  }
 
+  function handlingExpand() {
+    console.log('folder expanded');
+  }
 
   return (
     <div>
       <Tree.DirectoryTree
         multiple
         defaultExpandAll
-        onSelect={onSelect}
-        onExpand={onExpand}
-        treeData={treeData}
+        onSelect={handlingSelect}
+        onExpand={handlingExpand}
+        treeData={pages}
       />
     </div>
   );
