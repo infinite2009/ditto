62b55215df018e297ec157aa963b6b24a110a89e
luodongyang <luodongyang@bilibili.com>
2023-08-21 15:16:42 +0800

    chore: redo editor wrapper

--- src/pages/editor/edit-wrapper/index.tsx
+++ src/pages/editor/edit-wrapper/index.tsx
@@ -1,15 +1,55 @@
-import { ReactNode } from 'react';
-export interface IEditorWrapper {
-  name: string;
-  children: ReactNode;
+import {useCombinedRefs} from '@dnd-kit/utilities';
+import React, {CSSProperties} from 'react';
+import {useDraggable, useDroppable} from '@dnd-kit/core';
+
+export interface IEditorProps {
+  id: string;
+  childrenId?: string[];
+  children: React.ReactNode;
+  direction?: 'row' | 'column';
 }
 
-export default function EditWrapper({ name, children }: IEditorWrapper) {
+export default function EditWrapper({
+  id,
+  childrenId,
+  children,
+  direction = 'column',
+}: IEditorProps) {
+  const {isOver, setNodeRef: setDroppableNodeRef} = useDroppable({
+    id,
+    data: {
+      childrenId,
+      direction: direction || 'column',
+    },
+  });
+  const {
+    attributes,
+    setNodeRef: setDraggableNodeRef,
+    listeners,
+    isDragging,
+  } = useDraggable({
+    id,
+    data: {
+      childrenId,
+    },
+  });
+  const setNodeRef = useCombinedRefs(setDroppableNodeRef, setDraggableNodeRef);
 
+  const style: CSSProperties = {
+    // transform: CSS.Transform.toString(transform),
+    cursor: 'grab',
+    opacity: isDragging ? 0.5 : 1,
+    outline: isOver ? '2px solid #7193f1' : undefined,
+    transition: 'border 0.5s ease-in-out',
+    boxSizing: 'border-box',
+    display: 'flex',
+    flexDirection: direction || 'column',
+    margin: 10
+  };
 
-  // drop(divRef);
-
-  return children ? (
-    null
-  ) : <div>无效的组件</div>;
+  return (
+    <div ref={setNodeRef} {...listeners} {...attributes} style={style}>
+      {children}
+    </div>
+  );
 }
