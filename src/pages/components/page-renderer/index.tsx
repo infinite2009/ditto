425ebb6af314bd030696c241dbca028e40ceb933
luodongyang <luodongyang@bilibili.com>
2023-08-21 21:35:51 +0800

    fix: add key

--- src/pages/components/page-renderer/index.tsx
+++ src/pages/components/page-renderer/index.tsx
@@ -118,7 +118,7 @@ export default function PageRenderer(props: IPageRendererProps) {
         {childrenTemplate}
       </Component>
     );
-    return mode === 'edit' ? <EditWrapper id={id} >{tpl}</EditWrapper> : tpl;
+    return mode === 'edit' ? <EditWrapper key={id} id={id} >{tpl}</EditWrapper> : tpl;
   }
 
   return dsl ? <div>{recursivelyRenderTemplate(dsl.child)}</div> : null;
