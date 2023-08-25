1b2d88ffb61dc1a9bc3d0b5946873b292edc4ee7
luodongyang <luodongyang@bilibili.com>
2023-08-25 19:59:47 +0800

    chore: fecs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,20 @@
-const routes = [{
+const routes = [
+  {
     path: '/project-manage',
     name: '项目管理'
-}, {
+  },
+  {
     path: '/page-edit',
     name: '页面编辑'
-}, {
+  },
+  {
     path: '/page-preview',
     name: '预览'
-}, {
+  },
+  {
     path: '/home',
     name: '首页'
-}]
+  }
+];
 
 export default routes;
