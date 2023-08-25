114cb49d00cb20db9187e9cfcb6f2652e71b3efd
luodongyang <luodongyang@bilibili.com>
2023-08-25 17:37:15 +0800

    chore: make props, actions, events, handlers

--- src/types/page.schema.ts
+++ src/types/page.schema.ts
@@ -16,7 +16,7 @@ export default interface IPageSchema extends ISchema {
   // 页面名
   name: string;
   // 组件的属性
-  props: {
+  props?: {
     [key: ComponentId]: DynamicObject<IPropsSchema>;
   };
   // 页面的模板
@@ -28,13 +28,13 @@ export default interface IPageSchema extends ISchema {
   // 后端接口调用可能存在复用，所以组件的 handler action 需要引用这里的服务
   httpService?: DynamicObject<IHttpServiceSchema>;
   // 动作函数：转移状态、变换和格式化数据（最好放到 BFF 层），调用服务
-  actions: DynamicObject<IActionSchema>;
+  actions?: DynamicObject<IActionSchema>;
   // 页面内的事件，页面载入、退出、滚动、周期性事件
-  events: {
+  events?: {
     onload: IEventSchema;
     onunload: IEventSchema;
     interval: IEventSchema;
   };
   // 事件处理器
-  handlers: DynamicObject<IHandlerSchema>;
+  handlers?: DynamicObject<IHandlerSchema>;
 }
