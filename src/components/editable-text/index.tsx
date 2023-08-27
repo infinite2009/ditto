82a3e22346ab3d24463313fedf653c5761019582
infinite2009 <455429698@qq.com>
2023-08-27 16:26:43 +0800

    chore: add editable text

--- src/components/editable-text/index.tsx
+++ src/components/editable-text/index.tsx
@@ -1,3 +1,27 @@
-export default function EditableText() {
-  return <div>Editable Text works!</div>;
+import { useMemo } from 'react';
+
+export interface IEditableTextProps {
+  fontSize?: number;
+  fontWeight?: number;
+  lineHeight?: number;
+  color?: string;
+  children: string;
+}
+
+export default function EditableText({
+  fontSize = 14,
+  fontWeight = 400,
+  lineHeight = 14,
+  color = '#000',
+  children
+}: IEditableTextProps) {
+  const style = useMemo(() => {
+    return {
+      fontSize,
+      fontWeight,
+      lineHeight: `${lineHeight}px`,
+      color
+    };
+  }, [fontSize, fontWeight, lineHeight, color]);
+  return <div style={style}>{children}</div>;
 }
