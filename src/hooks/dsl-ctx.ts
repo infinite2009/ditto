683e391d6338725c7b758ef7c8a994c4bd13b439
infinite2009 <455429698@qq.com>
2023-08-27 11:18:31 +0800

    refactor: remove unused context

--- src/hooks/dsl-ctx.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-import DslProcessor from '@/service/dsl-process';
-
-const DSLContext = createContext<DslProcessor>(new DslProcessor());
-
-export default DSLContext;
