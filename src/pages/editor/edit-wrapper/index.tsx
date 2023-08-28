9467bc363ed1dcc631440c59c68559c0ff0b16e6
infinite2009 <455429698@qq.com>
2023-08-28 23:42:41 +0800

    chore: task_id=1156867193003071659 add outline offset to fix display issue

--- src/pages/editor/edit-wrapper/index.tsx
+++ src/pages/editor/edit-wrapper/index.tsx
@@ -36,6 +36,7 @@ export default function EditWrapper({ id, childrenId, children, direction = 'col
     // cursor: 'grab',
     opacity: isDragging ? 0.5 : 1,
     outline: isOver ? '2px solid #7193f1' : undefined,
+    outlineOffset: isOver ? 2 : undefined,
     transition: 'border 0.5s ease-in-out',
     boxSizing: 'border-box',
     display: 'flex',
