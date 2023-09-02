e0150529d54574095c9654379955c1198db04761
infinite2009 <455429698@qq.com>
2023-09-02 13:29:55 +0800

    chore: task_id=1156867193003071659 modify display

--- src/pages/editor/edit-wrapper/index.tsx
+++ src/pages/editor/edit-wrapper/index.tsx
@@ -1,5 +1,5 @@
 import { useCombinedRefs } from '@dnd-kit/utilities';
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { useDraggable, useDroppable } from '@dnd-kit/core';
 import ComponentFeature from '@/types/component-feature';
 
@@ -12,6 +12,8 @@ export interface IEditorProps {
 }
 
 export default function EditWrapper({ id, childrenId, children, direction = 'column', type }: IEditorProps) {
+  const [stylePartial, setStylePartial] = useState<CSSProperties>({});
+  const displayProcessed = useRef<boolean>(false);
   const { isOver, setNodeRef: setDroppableNodeRef } = useDroppable({
     id,
     data: {
@@ -32,28 +34,85 @@ export default function EditWrapper({ id, childrenId, children, direction = 'col
     }
   });
 
-  const style: CSSProperties = {
-    opacity: isDragging ? 0.5 : 1,
-    outline: isOver ? '2px solid #7193f1' : undefined,
-    outlineOffset: isOver ? -2 : undefined,
-    transition: 'border 0.5s ease-in-out',
-    boxSizing: 'border-box',
-  };
+  useEffect(() => {
+    const wrapperElement = document.getElementById(id);
+    if (!wrapperElement) {
+      return;
+    }
+    const childElement: HTMLElement = wrapperElement.children?.[0] as HTMLElement;
+    processBFC(wrapperElement, childElement);
+  }, []);
 
-  let setNodeRef;
+  function processBFC(targetElement: HTMLElement, childElement: HTMLElement) {
+    if (displayProcessed.current) {
+      return;
+    } else {
+      displayProcessed.current = true;
+      const display = childElement.style.display;
+      const width = childElement.style.width;
+      const flexBasis = childElement.style.flexBasis;
+      const floatReg = /^-?\d+(\.\d+)?$/;
+      switch (display) {
+        case 'block':
+          // 如果有具体宽度
+          if (width.indexOf('px') !== -1 && width.indexOf('%') !== -1 && floatReg.test(width)) {
+            targetElement.style.display = 'inline-block';
+          } else {
+            targetElement.style.display = 'block';
+          }
+          break;
+        case 'flex':
+          if (
+            (flexBasis.indexOf('px') !== -1 && flexBasis.indexOf('%') !== -1 && floatReg.test(flexBasis)) ||
+            (width.indexOf('px') !== -1 && width.indexOf('%') !== -1 && floatReg.test(width))
+          ) {
+            targetElement.style.display = 'inline-block';
+          } else {
+            targetElement.style.display = 'block';
+          }
+          break;
+        default:
+          targetElement.style.display = 'inline';
+          break;
+      }
+    }
+  }
+
+  const style: CSSProperties = useMemo(() => {
+    let backgroundColor;
+    switch (type) {
+      case ComponentFeature.slot:
+        backgroundColor = '#4f0';
+        break;
+      case ComponentFeature.container:
+        backgroundColor = '#fa0';
+        break;
+      default:
+        backgroundColor = '#0ff';
+        break;
+    }
+    return {
+      opacity: isDragging ? 0.5 : 1,
+      outline: isOver ? '2px solid #7193f1' : undefined,
+      outlineOffset: isOver ? -2 : undefined,
+      transition: 'border 0.5s ease-in-out',
+      boxSizing: 'border-box',
+      backgroundColor,
+      ...stylePartial
+    };
+  }, [type, stylePartial]);
+
+  let setNodeRef: React.LegacyRef<HTMLDivElement> | undefined;
   switch (type) {
     case ComponentFeature.slot:
       setNodeRef = setDroppableNodeRef;
-      style.backgroundColor = '#4f0';
       break;
     case ComponentFeature.container:
       setNodeRef = useCombinedRefs(setDroppableNodeRef, setDraggableNodeRef);
-      style.backgroundColor = '#fa0';
       break;
     default:
       // 为了让 dnd-kit 可以测量这个元素的尺寸，需要设置为 droppable
       setNodeRef = useCombinedRefs(setDroppableNodeRef, setDraggableNodeRef);
-      style.backgroundColor = '#0ff';
       break;
   }
 
