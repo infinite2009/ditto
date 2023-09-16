ac1574b735ca93588b69a32eca6981e34aa74486
luodongyang <luodongyang@bilibili.com>
2023-09-16 14:54:06 +0800

    chore: task_id=1156867193003071659 remove custom window control buttons

--- src/components/custom-title-bar/index.tsx
+++ src/components/custom-title-bar/index.tsx
@@ -1,23 +1,9 @@
-import { Button } from 'antd';
-import { appWindow } from '@tauri-apps/api/window';
+import styles from './index.module.less';
 
 export default function CustomTitleBar() {
-  function handleMaximize() {
-    appWindow.maximize();
-  }
-
-  function handleMinimize() {
-    appWindow.minimize();
-  }
-
-  function handleClose() {
-    appWindow.close();
-  }
   return (
-    <div data-tauri-drag-region>
-      <Button onClick={handleMaximize}>最大化</Button>
-      <Button onClick={handleMinimize}>最小化</Button>
-      <Button onClick={handleClose}>关闭</Button>
+    <div data-tauri-drag-region={true} className={styles.main}>
+      custom title bar works!
     </div>
   );
 }
