50f32e6cab093dff51ca7c06409591a04fdfe7b6
infinite2009 <455429698@qq.com>
2023-09-17 19:54:59 +0800

    chore: task_id=1156867193003071659 add event trigger enumeration

---
+++ src/types/event-trigger.ts
@@ -0,0 +1,9 @@
+enum EventTrigger {
+  onClick = 'onClick',
+  onDoubleClick = 'onDoubleClick',
+  onContextmenu = 'onContextmenu',
+  onMouseEnter = 'onMouseEnter',
+  onMouseLeave = 'onMouseLeave'
+}
+
+export default EventTrigger;
