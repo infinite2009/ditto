80df750ffc418e0b32f610e9f5fe66f515e173c4
infinite2009 <455429698@qq.com>
2023-09-23 23:06:29 +0800

    refactor: task_id=1156867193003071659 style props

--- src/components/editable-text/index.tsx
+++ src/components/editable-text/index.tsx
@@ -1,27 +1,10 @@
-import { useMemo } from 'react';
+import { CSSProperties, useMemo } from 'react';
 
 export interface IEditableTextProps {
-  fontSize?: number;
-  fontWeight?: number;
-  lineHeight?: number;
-  color?: string;
+  style: CSSProperties;
   children: string;
 }
 
-export default function EditableText({
-  fontSize = 14,
-  fontWeight = 400,
-  lineHeight = 14,
-  color = '#000',
-  children
-}: IEditableTextProps) {
-  const style = useMemo(() => {
-    return {
-      fontSize,
-      fontWeight,
-      lineHeight: `${lineHeight}px`,
-      color
-    };
-  }, [fontSize, fontWeight, lineHeight, color]);
+export default function EditableText({ style, children }: IEditableTextProps) {
   return <div style={style}>{children}</div>;
 }
