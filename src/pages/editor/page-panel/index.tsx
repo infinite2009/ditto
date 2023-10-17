2bf4703b1e9d2feea401f50d7353eaba14206ebb
infinite2009 <455429698@qq.com>
2023-10-17 15:20:29 +0800

    feat: task_id=1156867193003092459 add project toolbar

--- src/pages/editor/page-panel/index.tsx
+++ src/pages/editor/page-panel/index.tsx
@@ -2,6 +2,9 @@ import { Tree } from 'antd';
 import { DataNode, EventDataNode } from 'antd/es/tree';
 import { Key, useCallback, useEffect, useMemo, useState } from 'react';
 import { DownOutlined, FileOutlined, FolderOpenOutlined, FolderOutlined } from '@ant-design/icons';
+import ProjectToolBar from '@/pages/editor/project-tool-bar';
+
+import styles from './index.module.less';
 
 interface PageData {
   key: string;
@@ -90,18 +93,31 @@ export default function PagePanel({ data = [], selected, onSelect }: IPagePanel)
     setExpandedKeys(expandedKeys as string[]);
   }
 
+  function handleCreatingPage() {}
+
+  function handleCreatingProject() {}
+
+  function handleSearchingPage() {}
+
   return (
     <div>
+      <ProjectToolBar
+        onCreatingPage={handleCreatingPage}
+        onCreatingProject={handleCreatingProject}
+        onSearch={handleSearchingPage}
+      />
       {dataWithIcon?.length > 0 ? (
-        <Tree.DirectoryTree
-          switcherIcon={<DownOutlined />}
-          showIcon
-          selectedKeys={[selected]}
-          onExpand={handleExpand}
-          expandedKeys={expandedKeys}
-          onSelect={handlingSelect}
-          treeData={dataWithIcon}
-        />
+        <div className={styles.projectTree}>
+          <Tree.DirectoryTree
+            switcherIcon={<DownOutlined />}
+            showIcon
+            selectedKeys={[selected]}
+            onExpand={handleExpand}
+            expandedKeys={expandedKeys}
+            onSelect={handlingSelect}
+            treeData={dataWithIcon}
+          />
+        </div>
       ) : null}
     </div>
   );
