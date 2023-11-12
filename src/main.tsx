4e1578c11658fa6083d134e705ac70902ffabe83
infinite2009 <455429698@qq.com>
2023-11-12 12:27:30 +0800

    refactor: task_id=1156867193003092459 remove strict mode on non-Mac operating system

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.module.less';
-import { isMac } from '@/util';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  (await isMac()) ? (
-    <App />
-  ) : (
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
