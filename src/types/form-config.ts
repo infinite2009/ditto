27be487d2f2939d9207c53b9e2955fed68c4b124
infinite2009 <455429698@qq.com>
2023-12-02 16:06:44 +0800

    chore: task_id=1156867193003092459 add form name mapping

--- src/types/form-config.ts
+++ src/types/form-config.ts
@@ -1,12 +1,12 @@
 import { FC } from 'react';
 
 export interface FormItemSchema {
-  name: string;
   component: string;
   componentProps?: {
     [key: string]: any;
   };
   initialValue?: any;
+  name: string;
   // // 这个 form 配置项将合并到哪个 props 上，此时该 props 是一个对象，如果没有这个配置，则不进行合并
   // propsToCompose?: string;
   // 如果有这个
@@ -18,6 +18,8 @@ export interface FormItemSchema {
      * aggregation 表示聚合映射，就是说当前的表单值是目标 props 的一个属性
      */
     type: 'parallel' | 'aggregation';
+    // 函数实现的字符串，输入是当前表单值，输出是属性需要的值，是否聚合还是由 type 决定。例如：(name, val) => { return val }
+    funcStr: string;
   };
   required?: boolean;
   title: string;
