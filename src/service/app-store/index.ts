5087f59e421b22ce81f4d32c85a5cfa54287c96f
infinite2009 <455429698@qq.com>
2023-12-12 23:33:04 +0800

    refactor: task_id=1156867193003092459 comment some code

--- src/service/app-store/index.ts
+++ src/service/app-store/index.ts
@@ -72,13 +72,7 @@ export default class AppStore {
     makeAutoObservable(this);
   }
 
-  /**
-   * 切换场景
-   * @param newScene
-   */
-  switchScene(newScene: Scene) {
-    this.scene = newScene;
-  }
+  // invokeHandler(shortKey: string, scene: Scene) {}
 
   /**
    * 给指定场景下指定快捷键注册处理器
@@ -86,17 +80,25 @@ export default class AppStore {
    * @param scene
    * @param handler
    */
-  registerShortKey(shortKey: string, scene: Scene, handler: (e: { shortKey: string; scene: Scene }) => void) {
-    // 检测到当前场景下的快捷键被其他的处理器注册，抛出错误。允许同一个handler反复注册，保持幂等
-    if (this.sceneDict[scene]?.[shortKey] && this.sceneDict[scene]?.[shortKey] !== handler) {
-      throw new Error('当前快捷键已被使用，请更换另外一个快捷键');
-    }
-    // 如果当前场景没有初始化
-    if (!this.sceneDict[scene]) {
-      this.sceneDict[scene] = {};
-    }
-    // 注册当前的 handler
-    this.sceneDict[scene][shortKey] = handler;
+  // registerShortKey(shortKey: string, scene: Scene, handler: (e: { shortKey: string; scene: Scene }) => void) {
+  //   // 检测到当前场景下的快捷键被其他的处理器注册，抛出错误。允许同一个handler反复注册，保持幂等
+  //   if (this.sceneDict[scene]?.[shortKey] && this.sceneDict[scene]?.[shortKey] !== handler) {
+  //     throw new Error('当前快捷键已被使用，请更换另外一个快捷键');
+  //   }
+  //   // 如果当前场景没有初始化
+  //   if (!this.sceneDict[scene]) {
+  //     this.sceneDict[scene] = {};
+  //   }
+  //   // 注册当前的 handler
+  //   this.sceneDict[scene][shortKey] = handler;
+  // }
+
+  /**
+   * 切换场景
+   * @param newScene
+   */
+  switchScene(newScene: Scene) {
+    this.scene = newScene;
   }
 
   /**
@@ -109,6 +111,4 @@ export default class AppStore {
       delete this.sceneDict[scene][shortKey];
     }
   }
-
-  invokeHandler(shortKey: string, scene: Scene) {}
 }
